8d883330e44d7de3c8d1f724b9ca8c48f03db044
vlaksi <daksrapsody@gmail.com>
2020-04-14 18:49:00 +0200

    list candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,21 +1,40 @@
 pragma solidity ^0.5.16;	// this is version of solidity we are using
 
 contract Election{
-	// Store candidate
-    // Read candidate
-    string public candidate;
- 	// candidate is 'state variable' and it's something that's
-    // going to e accessible inside of our contact and is going
-    // to represent data that belongs to our entire contact
 
-    // in solidity we get geter when we create state variable
-    // so, no need for creating one
-
-
-    // Constructor, he is going to get run whenever we deploy our
-    // contract to the blockchain, and because of that we set public
-    constructor() public {
-    	candidate = "Candidate 1";
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
     }
 
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // key is uint and value is our candidate
+    // mapping is like hashtable
+
+
+    // Store Candidates Count
+    uint public candidatesCount;
+    // scope of this state variable is in contracts Election
+
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+    	// because _name is local variable, we use underscore _before_var_name
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+    /*
+		When we created struct model Candidate,
+		we have 'built-in constructor', so when we want
+		to make one instance of that model
+		we go in order for each variable in model.
+		Candidate(firstVariable, secondVariable, and so on...) 
+    */
 }
